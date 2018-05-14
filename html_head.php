74bef6b66ed894147ebdf84a0c2fbf9ac7fa66d5
Juhanj <juhani.jarvio@gmail.com>
2018-05-14 17:26:16 +0300

    Lisää HTML basic components
    head, header, ja footer. PHP-tiedostoja, koska reasons.

---
+++ html_head.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1); ?>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Kassax</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="./css/main.css">
+	<!-- LINK TO BOOTSTRAP -->
+</head>
