5c532cb584df42f543095d8300266b28566c108e
Juhanj <juhani.jarvio@gmail.com>
2018-05-20 13:46:39 +0300

    Add basic template code to admin index

---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+require '_start.php'; global $db, $user;
+
+tarkista_admin( $user );
+tarkista_feedback_POST();
+?>
+
+<!DOCTYPE html>
+<html lang="fi">
+
+<?php require 'html_head.php'; ?>
+
+<body>
+
+<?php require 'html_header.php'; ?>
+
+<main class="main_body_container">
+
+
+
+</main>
+
+<?php require 'html_footer.php'; ?>
+
+<script>
+</script>
+
+</body>
+</html>
