c7616f86ee12b523c31c727581849baaf3f737ac
Juhanj <juhani.jarvio@gmail.com>
2018-05-24 16:39:52 +0300

    Update config.cfg
    - Admin-osion siivousta.
    - Tecdoc-koodi siirretty omaan osioon.
    - Misc-osio lisätty.

--- cfg/config.ini.php
+++ cfg/config.ini.php
@@ -8,13 +8,30 @@ name = "kassax_database"
 user = "root"
 pass = ""
 
+[TecDoc]
+tecdoc_koodi = "10"
+
+[Misc]
+indev = "0"
+indev_txt = "<p>Kassax inDev testing."
+update = "0"
+update_txt = "<p></p><p>YYYY-MM-DD hh:mm</p>"
+header_tervehdys = "<span style='color: red;'>InDev testing</span>"
+
 [Admin]
-; Poista tämä osio asennuksen jälkeen
-kayttajatunnus[] = ""
-salasana[] = ""
-y_tunnus = "2769696-8"
-y_yritystunniste = "osax"
-y_nimi = "Osax Oy"
+kayttajatunnus = "admin@admin"
+salasana = "admin"
+
+y_tunnus = "1234567-8"
+y_yritystunniste = "admin"
+y_nimi = "Admin"
+y_katuosoite = "Adminstreet 52"
+y_postinumero = "00800"
+y_postitoimipaikka = "JOENSUU"
+y_maa = "Suomi"
+y_puhelin = "000-1234-567"
+y_www_url = "www.admin.yp"
+y_sahkoposti = "admin@admin"
 
 ;*/
 ;?>
