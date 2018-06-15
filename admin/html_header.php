7ebff2262557fa250c8a698bd779b8820b42d000
samileh <smai_lehtinen@hotmail.com>
2018-06-15 19:53:50 +0300

    Minor fixes

--- admin/html_header.php
+++ admin/html_header.php
@@ -4,18 +4,47 @@
 	<meta http-equiv="refresh" content="0; url=index.php">
 </noscript>
 
-<header class="header_container">
+<style>
+	#headertop { background-color: white; }
+</style>
 
-	<section class="header_top" id="headertop">
-		<div id="head_logo">
-			<a href="./index.php"> <img src="./../img/kassax_logo.jpg" align="left"> </a>
+<header>
+
+	<section class="d-flex flex-row align-items-center" id="headertop">
+		<div>
+			<a href="./index.php"> <img src="./../img/kassax_logo.jpg"> </a>
 		</div>
-
-		<div id="head_info">
+		<div class="container" id="head_info">
 			WIP HEADER TESTING
 		</div>
 	</section>
 
-	<nav class="navigationbar" id="navbar">
+	<nav class="navbar navbar-expand-md navbar-light bg-light">
+		<!-- Brand & hamburger-icon -->
+		<a class="navbar-brand" href="./index.php">
+			<i class="material-icons" style="margin-top: -3px;">home</i>
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<!-- Left -->
+		<div class="collapse navbar-collapse">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#">Asiakkaat</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Raportit</a>
+				</li>
+			</ul>
+		</div>
+		<!-- Right -->
+		<div class="collapse navbar-collapse">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="./../logout.php">Logout</a>
+				</li>
+			</ul>
+		</div>
 	</nav>
 </header>
