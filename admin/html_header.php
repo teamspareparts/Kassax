20db73ceda9744cd542e5a3022f052c54fb626f2
Juhanj <juhani.jarvio@gmail.com>
2018-07-09 11:30:39 +0300

    Changes to navbar look
    Kommentoitu pois brand-kohta, mutta koodi jätetty jos sille tulee tarvetta.
    
    Lisätty dropdown, paitsi jos se oli siellä jo, missä tapauksessa, päivitetty dropdownin sisältöä hieman.
    
    Korvattu osaxin "Kirjaudu ulos" dropdownilla, jossa asioita. Oli puhe jossain vaiheessa korvata se ikonilla, jossa olisi linkki Omat tiedot -sivulle, joten tässä on.
    
    CSS: logout-linkin hover värjää punaiseksi. Erottuu joukosta.
    
    Tämän muutoksen tarkoitus on vain tarjota kaikki toiminnallisuus, jota me tarvitsemme. Mahdollinen tyylittely, ja lisäominaisuudet myöhemmin. Esim. toimivat linkit, eikä vain placeholdereita.

--- admin/html_header.php
+++ admin/html_header.php
@@ -6,10 +6,11 @@
 
 <header>
 
-	<section class="d-flex flex-row align-items-center hidden"
-		style="background-color: white;">
+	<section class="d-flex flex-row align-items-center hidden" style="background-color: white;">
 		<div>
-			<a href="./index.php"> <img src="./../img/kassax_logo.jpg" height="30px"> </a>
+			<a href="./index.php">
+				<img src="./../img/kassax_logo.jpg" height="30">
+			</a>
 		</div>
 		<div class="container">
 			<?= $lang->HEADER_WELCOME ?>
@@ -17,16 +18,18 @@
 	</section>
 
 
-	<nav class="navbar navbar-expand-md navbar-light bg-light">
+	<nav class="navbar navbar-expand-md navbar-dark" style="background-color: #2f5cad;">
+
 		<!-- Brand & hamburger-icon -->
-		<a class="navbar-brand" href="./index.php">
-			<img src="./../img/kassax_logo.jpg" style="height: 27px;">
+<!--		<a class="navbar-brand" href="./index.php">-->
+<!--			<img src="./../img/kassax_logo.jpg" style="height: 27px;">-->
 <!--			<i class="material-icons" style="margin-top: -3px;">home</i>-->
-		</a>
+<!--		</a>-->
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".collapse"
 		        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
+
 		<!-- Left -->
 		<div class="collapse navbar-collapse">
 			<ul class="navbar-nav mr-auto">
@@ -36,16 +39,57 @@
 				<li class="nav-item">
 					<a class="nav-link" href="#"><?= $lang->NAV_ITEM_2 ?></a>
 				</li>
-			</ul>
-		</div>
-		<!-- Right -->
-		<div class="collapse navbar-collapse">
-			<ul class="navbar-nav ml-auto">
-				<li class="nav-item">
-					<a class="nav-link" href="./../logout.php"><?= $lang->NAV_LOGOUT ?></a>
+
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+					   aria-haspopup="true" aria-expanded="false">
+
+						<i class="material-icons">
+							more_vert
+						</i>
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						<a class="dropdown-item" href="#"><?= $lang->NAV_TEST_0 ?></a>
+						<a class="dropdown-item" href="#"><?= $lang->NAV_TEST_0 ?></a>
+						<hr>
+						<a class="dropdown-item" href="#"><?= $lang->NAV_TEST_0 ?></a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#"><?= $lang->NAV_TEST_0 ?></a>
+						<a class="dropdown-item" href="#"><?= $lang->NAV_TEST_0 ?></a>
+					</div>
 				</li>
 			</ul>
 		</div>
+
+		<!-- Right -->
+		<ul class="navbar-nav ml-auto">
+
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+				   aria-haspopup="true" aria-expanded="false">
+					<i class="material-icons">
+						account_box
+					</i>
+				</a>
+
+				<div class="dropdown-menu dropdown-menu-right"
+				     aria-labelledby="navbarDropdownMenuLink">
+
+					<a class="dropdown-item" href="#"><?= $lang->NAV_TEST_0 ?></a>
+					<a class="dropdown-item" href="#"><?= $lang->NAV_TEST_0 ?></a>
+					<hr>
+					<a class="dropdown-item nav-logout" href="./../logout.php">
+						<?= $lang->NAV_LOGOUT ?>
+						<i class="material-icons">
+							exit_to_app
+						</i>
+					</a>
+				</div>
+			</li>
+
+			<li class="nav-item">
+			</li>
+		</ul>
 	</nav>
 
 </header>
