de97fd3eb4861f98646f2bec4ce2ecc0fe124c59
Juhanj <juhani.jarvio@gmail.com>
2018-07-19 02:27:36 +0300

    Fix / suggestion / ideas / random nonsense for the input inside label tag thing
    See comments around nimi- and Y-tunnus inputs.
    
    But yeah, sain inputin toimimaan labelin sisällä samalla tavalla.
    
    Voit vapaasti poistaa tämä päivityksen jos haluat nopeasti ne divit takaisin.

--- admin/company_add.php
+++ admin/company_add.php
@@ -29,68 +29,82 @@ if ( isset($_POST['add']) ) {
 
 <div class="container">
 	<form method="post" class="needs-validation" id="xxx" novalidate>
-		<div class="form-group">
-			<label for="nimi">Nimi</label>
+
+		<!-- div changed to label, changed CSS to make block element -->
+		<label class="form-group">
+			<!-- label to span, added CSS: label (inline-block & margin-bottom) & required (red *) -->
+			<span class="label required">Nimi</span>
+			<!-- Input not changed -->
 			<input type="text" name="nimi" class="form-control" id="nimi" placeholder="Nimi">
-		</div>
-		<div class="form-group">
-			<label for="">Y-tunnus</label>
-			<input type="text" name="y-tunnus" class="form-control" id="y-tunnus" placeholder="Y-tunnus" pattern="[0-9]{6,7}-[0-9]" required>
-			<div class="invalid-feedback">
+		</label>
+
+		<label class="form-group">
+			<!-- You can also leave the label there, because apparently label inside label is just fine ¯\_(ツ)_/¯ -->
+			<!-- But for="" here is still mandatory, koska muuten hiiren klikkaus suoraan labelin päällä ei toimi -->
+			<label for="ytun" class="required">Y-tunnus</label>
+			<input type="text" name="y-tunnus" class="form-control" id="ytun" placeholder="Y-tunnus"
+			       pattern="[0-9]{6,7}-[0-9]" required>
+
+			<!-- feedback div changed to span, seems to make no difference.
+				Block elements not alloved inside label -->
+			<span class="invalid-feedback">
 				<?php //TODO : Feedback string from db
 					echo "invalid"
 				?>
-			</div>
-		</div>
-		<div class="form-group">
-			<label for="yritystunniste">Yritystunniste</label>
-			<input type="text" name="yritystunniste" class="form-control" id="yritystunniste" placeholder="Yritystunniste" minlength="3" required>
-			<div class="invalid-feedback">
+			</span>
+		</label>
+
+		<label class="form-group">
+			<span class="label required">Yritystunniste</span>
+			<input type="text" name="yritystunniste" class="form-control" placeholder="Yritystunniste" minlength="3" required>
+			<span class="invalid-feedback">
 				<?php //TODO : Feedback string from db
 					echo "Min length: 3"
 				?>
-			</div>
-		</div>
-		<div class="form-group">
-			<label for="katuosoite">Katuosoite</label>
-			<input type="text" name="katuosoite" class="form-control" id="katuosoite" placeholder="Katuosoite">
-		</div>
+			</span>
+		</label>
+
+		<label class="form-group">
+			<span class="label">Katuosoite</span>
+			<input type="text" name="katuosoite" class="form-control" placeholder="Katuosoite">
+		</label>
+
 		<div class="row">
 			<div class="col-sm">
-				<div class="form-group">
+				<label class="form-group">
 					<label for="postinumero">Postinumero</label>
 					<input type="text" name="postinumero" class="form-control" id="postinumero" placeholder="Postinumero">
-				</div>
+				</label>
 			</div>
 			<div class="col-sm">
-				<div class="form-group">
+				<label class="form-group">
 					<label for="postitoimipaikka">Postitoimipaikka</label>
 					<input type="text" name="postitoimipaikka" class="form-control" id="postitoimipaikka" placeholder="Postitoimipaikka">
-				</div>
+				</label>
 			</div>
 			<div class="col-sm">
-				<div class="form-group">
+				<label class="form-group">
 					<label for="maa">Maa</label>
 					<input type="text" name="maa" class="form-control" id="maa" placeholder="Maa">
-				</div>
+				</label>
 			</div>
 		</div>
-		<div class="form-group">
+		<label class="form-group">
 			<label for="puhelinnumero">Puhelinnumero</label>
 			<input type="text" name="puhelinnumero" class="form-control" id="puhelinnumero" placeholder="Puhelinnumero">
-		</div>
-		<div class="form-group">
+		</label>
+		<label class="form-group">
 			<label for="www">www-osoite</label>
 			<input type="text" name="www" class="form-control" id="www" placeholder="www-osoite">
-		</div>
-		<div class="form-group">
+		</label>
+		<label class="form-group">
 			<label for="email">Sähköpostiosoite</label>
 			<input type="text" name="email" class="form-control" id="email" placeholder="Sähköpostiosoite">
-		</div>
-		<div class="form-group">
+		</label>
+		<label class="form-group">
 			<label for="logo">Logotiedosto</label>
 			<input type="text" name="logo" class="form-control" id="logo" placeholder="tiedosto tai url">
-		</div>
+		</label>
 		<button type="submit" name="add" class="btn btn-primary">Submit</button>
 	</form>
 </div>
